0a7027fb998181e67c4fa570acd6c8f816136f46
jake.0208 <jake.0208@kakaocorp.com>
2022-03-08 23:05:58 +0900

    FEAT: Fix routing part(except authors)

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
+const http = require('http');
+const url = require('url');
+const topic = require('./lib/topic');
+const author = require('./lib/author');
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+//route, routing
+app.get('/', (request, response) => {
+    topic.home(request, response);
+})
+app.get('/page/:pageId', (request, response) => {
+    topic.page(request, response);
+})
+app.get('/create', (request, response) => {
+    topic.create(request, response);
+})
+app.post('/create_process', (request, response) => {
+    topic.create_process(request, response);
+})
+app.get('/update/:pageId', (request, response) => {
+    topic.update(request, response);
+})
+app.post('/update_process', (request, response) => {
+    topic.update_process(request, response);
+})
+app.post('/delete_process', (request, response) => {
+    topic.delete(request, response);
 })
 
 app.listen(port, () => {
@@ -12,32 +35,12 @@ app.listen(port, () => {
 
 
 /*
-var http = require('http');
-var url = require('url');
-var topic = require('./lib/topic');
-var author = require('./lib/author');
 
 var app = http.createServer(function (request, response) {
             var _url = request.url;
             var queryData = url.parse(_url, true).query;
             var pathname = url.parse(_url, true).pathname;
 
-            if (pathname === '/') {
-                if (queryData.id == undefined) {
-                    topic.home(request, response);
-                } else {
-                    topic.page(request, response);
-                }
-            } else if (pathname === '/create') {
-                topic.create(request, response);
-            } else if (pathname === '/create_process') {
-                topic.create_process(request, response);
-            } else if (pathname === '/update') {
-                topic.update(request, response);
-            } else if (pathname === '/update_process') {
-                topic.update_process(request, response);
-            } else if (pathname === '/delete_process') {
-                topic.delete(request, response);
             } else if (pathname === '/author') {
                 author.home(request, response);
             } else if (pathname === '/author/create_process') {
